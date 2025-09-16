77053038217896211c5423417b69b613afdc3006
Lucas Albuquerque <lucasnalb3010@gmail.com>
2025-09-16 12:07:27 -0300

    Update Instance.h
    - Métodos públicos e privados adicionados

--- Instance.h
+++ Instance.h
@@ -11,6 +11,34 @@ private:
     int n; // numero de estacoes (o deposito eh o vertice 0, entao os vertices totais são n+1)
     int m; // numero max de veiculos
     int Q; // capacidade dos veiculos
+
+    vector<int> d; // Lista de demandas. d[0] eh a demanda do depósito (sempre 0), d[1] ate d[n] são as demandas das estacoes 1 a n.
+    vector<vector<int>> c; // Matriz de custos (distancias). c[i][j] = custo de ir do vertice i para o vertice j.
+
+public:
+    // Construtor
+    Instance(const string& filename);
+
+    // Getters
+    int getNumStations() const { 
+        return n;
+    }
+    int getNumVehicles() const { 
+        return m;
+    }
+    int getVehicleCapacity() const { 
+        return Q;
+    }
+    int getDemand(int vertex) const { 
+        return d[vertex];
+    }
+
+    int getCost(int i, int j) const 
+    { 
+        return c[i][j]; }
+
+    // Imprime os dados da instancia
+    void print() const;
 };
 
 #endif
